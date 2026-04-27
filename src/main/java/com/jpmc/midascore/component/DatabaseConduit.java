104458809532fa4d0f823989143b0298680fcfc9
Preet Patel <preetpatel1862@gmail.com>
2026-04-26 23:55:39 -0400

    Task Four Completed

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -2,19 +2,30 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
+    private final String incentiveApiUrl;
 
-    public DatabaseConduit(UserRepository userRepository, TransactionRepository transactionRepository) {
+    public DatabaseConduit(UserRepository userRepository,
+                           TransactionRepository transactionRepository,
+                           RestTemplateBuilder restTemplateBuilder,
+                           @Value("${general.incentive-api-url}") String incentiveApiUrl) {
         this.userRepository = userRepository;
         this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplateBuilder.build();
+        this.incentiveApiUrl = incentiveApiUrl;
     }
 
     public void save(UserRecord userRecord) {
@@ -30,10 +41,12 @@ public class DatabaseConduit {
         if (sender.getBalance() < transaction.getAmount()) {
             return;
         }
+        Incentive incentive = restTemplate.postForObject(incentiveApiUrl, transaction, Incentive.class);
+        float incentiveAmount = incentive == null ? 0f : Math.max(0f, incentive.getAmount());
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
         userRepository.save(sender);
         userRepository.save(recipient);
-        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount));
     }
 }
--- src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -21,13 +21,16 @@ public class TransactionRecord {
 
     private float amount;
 
+    private float incentive;
+
     protected TransactionRecord() {
     }
 
-    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount, float incentive) {
         this.sender = sender;
         this.recipient = recipient;
         this.amount = amount;
+        this.incentive = incentive;
     }
 
     public Long getId() {
@@ -44,5 +47,9 @@ public class TransactionRecord {
 
     public float getAmount() {
         return amount;
+    }
+
+    public float getIncentive() {
+        return incentive;
     }
 }
